bacda23023eb0792873d4a32e60920a0d4662479
Chris Castle <chrisjcastle93@gmail.com>
2022-02-16 19:05:36 +0100

    labs done without passport

--- app.js
+++ app.js
@@ -1,44 +1,68 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const MongoStore = require('connect-mongo');
+const session = require('express-session')
 
+const MONGODB_URI = "mongodb://127.0.0.1/lab-auth-with-passport";
+
+// Connection to the database "recipe-app"
 mongoose
-  .connect('mongodb://localhost/auth-with-passport')
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((x) => {
+    console.log(`Connected to the database: "${x.connection.name}"`);
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true },
+  store: MongoStore.create({
+    mongoUrl: MONGODB_URI
+})
+}))
 
 // Express View engine setup
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Express - Generated with IronGenerator";
 
 // Routes middleware goes here
-const index = require('./routes/index.routes');
-app.use('/', index);
-const authRoutes = require('./routes/auth.routes');
-app.use('/', authRoutes);
+const index = require("./routes/index.routes");
+app.use("/", index);
+const authRoutes = require("./routes/auth.routes");
+app.use("/", authRoutes);
 
 module.exports = app;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,68 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-// Require user model
+const User = require("../models/User.model");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+router.get("/signup", (req, res) => res.render("auth/signup"));
+router.get("/login", (req, res) => res.render("auth/login"));
+
+router.post("/signup", (req, res, next) => {
+  const { username, password } = req.body;
+  bcrypt
+    .genSalt(saltRounds)
+    .then((salt) => {
+      return bcrypt.hash(password, salt);
+    })
+    .then((hash) => {
+      const hashPass = hash;
+      return hashPass;
+    })
+    .then((hashPass) => {
+      return User.create({
+        username: username,
+        password: hashPass,
+      });
+    })
+    .then((response) => {
+      res.redirect("/");
+      console.log("NEW USER CREATED", response);
+    })
+    .catch((err) => console.log(err));
+});
 
 // Add bcrypt to encrypt passwords
 
+router.post("/login", (req, res, next) => {
+  const { username, password } = req.body;
+  User.findOne({ username })
+    .then((user) => {
+      console.log('finding user')
+      if (!user) {
+        console.log('no user found')
+        res.render("auth/login", {
+          errorMessage: "Email is not registered. Try with other email.",
+        });
+        return;
+      } else if (bcrypt.compareSync(password, user.password)) {
+        console.log('found user, password match')
+        req.session.currentUser = user;
+        console.log(req.session)
+        res.render("auth/private", { user });
+      } else {
+        console.log('found user, password match')
+        res.render("auth/login", { errorMessage: "Incorrect password." });
+      }
+    })
+    .catch((error) => next(error));
+});
+
 // Add passport
 
-const ensureLogin = require('connect-ensure-login');
+const ensureLogin = require("connect-ensure-login");
 
-router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render('passport/private', { user: req.user });
+router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("passport/private", { user: req.user });
 });
 
 module.exports = router;
